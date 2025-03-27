417b179ab331933f7c4251c23f79ddf0639591d8
Indra Gotemey <indrapgotamey.pce@rub.edu.bt>
2025-03-27 10:33:47 +0600

    multilevel

--- Day2/test2.py
+++ Day2/test2.py
@@ -16,7 +16,8 @@ class Car(Vehicle):
         self.seats = seats
 
     def display_info(self):
-        print(f"{self.brand} {self.model} {self.rental_price_per_day} {self.seats}")   
+        super().display_info()
+        print(f"{self.seats}")   
 
 class Bike(Vehicle):
     def __init__(self, brand, model, rental_price_per_day, engine_capacity):
@@ -24,12 +25,26 @@ class Bike(Vehicle):
         self.engine_capacity = engine_capacity
 
     def display_info(self):
-        print(f"{self.brand} {self.model} {self.rental_price_per_day} {self.engine_capacity}")
+        super().display_info()
+        print(f"{self.engine_capacity}")
+
+class Cycle(Bike):
+    def __init__(self, brand, model, rental_price_per_day, engine_capacity, gear_capacity):
+        super().__init__(brand, model, rental_price_per_day, engine_capacity)
+        self.gear_capacity = gear_capacity
+
+    def display_info(self):
+        super().display_info()
+        print(f"{self.gear_capacity}")
+
 
 carobj = Car('Toyota', 2025, 4500.5, 5)
 carobj.display_info()
 
-carobj = Car('Yamaha', 2024, 1500.5, 125)
+carobj = Bike('Yamaha', 2024, 1500.5, 125)
+carobj.display_info()
+
+carobj = Cycle('Hercules', 2023, 1500.25, 50, 100)
 carobj.display_info()
 
 
