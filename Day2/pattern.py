c00e08b8f8d5d756d52805541d64769ebd208128
Indra Gotemey <indrapgotamey.pce@rub.edu.bt>
2025-03-27 12:31:12 +0600

    pattern

--- Day2/pattern.py
+++ Day2/pattern.py
@@ -1,1 +1,37 @@
+class EventManager:
+    def __init__(self):
+        self.subscribers = []
 
+    def subscribe(self, observer):
+        self.subscribers.append(observer)
+
+    def unsubscribe(self, observer):
+        self.subscribers.remove(observer)
+
+    def notify(self, event_data):
+        for subscriber in self.subscribers:
+            subscriber.update(event_data)
+
+
+class EventListener:
+    def __init__(self, name):
+        self.name = name
+
+    def update(self, event_data):
+        print(f"{self.name} received event: {event_data}")
+
+
+if __name__ == "__main__":
+    event_manager = EventManager()
+
+    listener1 = EventListener("Listener 1")
+    listener2 = EventListener("Listener 2")
+
+    event_manager.subscribe(listener1)
+    event_manager.subscribe(listener2)
+
+    event_manager.notify("New Event: Data Updated!")
+
+    event_manager.unsubscribe(listener1)
+
+    event_manager.notify("New Event: User Logged In!")
